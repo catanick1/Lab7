31528231cc03b0f787eaaf41ddbe25380bae3304
Calvin Catania <catanick@mail.uc.edu>
2021-03-22 20:32:28 +0000

    commented

--- Counter.java
+++ Counter.java
@@ -1,18 +1,26 @@
+//created new file "Counter.java"
+//add all the imports
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Counter implements ActionListener {
+//declare instance variable
+public class Counter implements ActionListener 
+{
   JLabel leftLabel;
   JLabel rightLabel;
   int leftCount;
   int rightCount;
 
- Counter() {
+//counter constor
+ Counter() 
+ {
+   //declare & initialize, set the layout, set frame size
    JFrame frame = new JFrame("Let's Count");
    frame.setLayout(new FlowLayout());
    frame.setSize(200,110);
 
+   //declare and initialize JButton
    JButton left = new JButton("Left");
    JButton right = new JButton("Right");
 
@@ -22,32 +30,44 @@ public class Counter implements ActionListener {
    left.addActionListener(this);
    right.addActionListener(this);
 
-   frame.add(left);
-   frame.add(right);
-
+  // initialize labels
    leftLabel = new JLabel ("Count: " +leftCount);
-   rightLabel = new JLabel ("Count: "+rightCount);frame.add(leftLabel);
-   frame.add(rightLabel);
-
-   frame.setVisible(true);
-   
+   rightLabel = new JLabel ("Count: "+rightCount);
+  
+  //declare and initialize reset button
    JButton reset = new JButton("Reset");
    reset.addActionListener(this);
-   frame.add(reset);
-    }
 
-    public void actionPerformed(ActionEvent ae) {
-      if(ae.getActionCommand().equals("Left")){
+    //add all of the objects in order of LLabel, RLabel, L , Rbutton, reset   
+    frame.add(left);
+    frame.add(right);
+    frame.add(leftLabel);
+    frame.add(rightLabel);
+    frame.add(reset);
+
+    frame.setVisible(true);
+  }
+
+    //actionPerformed method just like the buttonDemo
+    //if left, increase left count
+    //else if right, increase right count
+    //else if reset, reset to zero
+    public void actionPerformed(ActionEvent ae) 
+    {
+      if(ae.getActionCommand().equals("Left"))
+      {
         leftCount++;
         leftLabel.setText("Count: " + leftCount);
       }
 
-      else if(ae.getActionCommand().equals("Right")){
+      else if(ae.getActionCommand().equals("Right"))
+      {
         rightCount++;
         rightLabel.setText("Count: " + rightCount);
       }
 
-      else {
+      else 
+      {
         rightCount = 0;
         leftCount = 0;
         leftLabel.setText("Count: " + leftCount);
--- Main.java
+++ Main.java
@@ -1,17 +1,19 @@
 //imports
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 class Main {
 
-public static void main (String args[]) {
+public static void main (String args[]) 
+{
   SwingUtilities.invokeLater(new Runnable()
   {
-    public void run() {
+    public void run() 
+    {
       new Counter();
     }
-  });
+  }
+  );
 }
 }
